10f735c8e9c724e46e9fe51e4ce5a00a22a17b51
Saad <sqfmason@yahoo.com>
2015-11-12 00:08:43 -0600

    tested stripe button with test api key

--- app/models/book.rb
+++ app/models/book.rb
@@ -18,6 +18,6 @@ class Book < ActiveRecord::Base
 	validates :image, attachment_presence: true 
 	validates :resource, attachment_presence: true 
     
-	validates_numericality_of :price
+	validates_numericality_of :price,
 	greater_than: 49, message: "must be at least 50 cents"
 end
